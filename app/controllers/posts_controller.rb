3901ab8415ea9b0c9868a8de494a4ea730f83df3
sean craig <sean.m.craig02@gmail.com>
2015-10-05 00:56:13 -0600

    backup

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,46 @@
+class PostsController < ApplicationController
+	before_action :find_post, only: [:show, :edit, :update, :destroy]
+	def index
+	end
+	def new
+		@posts = Post.new
+	end
+	
+	def create
+		@posts = Post.new post_params
+
+		if @posts.save
+			redirect_to @posts, notice: "It saved!"
+		else
+			render 'new', notice: "Something went wrong!?!"
+		end
+	end
+
+	def show
+	end
+
+	def edit
+	end
+
+
+
+	def update
+		if @posts.update post_params
+			redirect_to @posts, notice: "It was saved!"
+		end
+	end
+
+	def destroy
+		@posts.destroy
+		redirect_to posts_path
+	end
+
+	private
+	def post_params
+		params.require(:post).permit(:tile, :content)
+	end
+
+	def find_post
+		@posts = Post.find(params[:id])
+	end		
+end
