7c29ffd6551aa5d437ae6c75b9fcc33c2aa34867
sean craig <sean.m.craig02@gmail.com>
2015-10-10 08:14:01 -0600

    index fix

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,8 +1,10 @@
 class PostsController < ApplicationController
-	before_action :find_post, only: [:index, :show, :edit, :update, :destroy]
+	before_action :find_post, only: [:show, :edit, :update, :destroy]
 	def index
 		@posts = Post.all.order("created_at desc").paginate(page: params[:id],per_page:5)
 	end
+
+
 	def new
 		@posts = Post.new
 	end
@@ -22,8 +24,6 @@ class PostsController < ApplicationController
 
 	def edit
 	end
-
-
 
 	def update
 		if @posts.update post_params
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ActiveRecord::Base
 
 	extend FriendlyId
-	friendly_id :tile, use: :slugged
+	friendly_id :tile, use: :slugged   
 	
 end
